34cc692edcee100339c9b4e1607792b6d97afba0
dredshep <dredshep@gmail.com>
2022-05-16 11:48:32 -0400

    testeroni

---
+++ pages/2.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function Second() {
+  return (
+    <Link href="/" shallow>
+      BIRB BIRB LOL
+    </Link>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,43 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { NextGA } from "next-ga4";
+import { AppProps } from "next/app";
+// ReactGA.initialize("G-5XP46M5HNL");
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: AppProps) {
+  // const router = useRouter();
+
+  // useEffect(() => {
+  //   console.log(router.pathname);
+  //   ReactGA.send({ hitType: "pageview", page: router.pathname });
+  //   const handleRouteChange = (
+  //     pathname: string,
+  //     { shallow }: { shallow: boolean }
+  //   ) => {
+  //     // REACTGA
+  //     // Send pageview with a custom path
+  //     ReactGA.send({ hitType: "pageview", page: pathname });
+
+  //     console.log(
+  //       `App is changing to ${pathname} ${
+  //         shallow ? "with" : "without"
+  //       } shallow routing`
+  //     );
+  //   };
+
+  //   router.events.on("routeChangeComplete", handleRouteChange);
+
+  //   // If the component is unmounted, unsubscribe
+  //   // from the event with the `off` method:
+  //   return () => {
+  //     router.events.off("routeChangeComplete", handleRouteChange);
+  //   };
+  // }, [router.events, router.pathname]);
+
+  return (
+    <>
+      <NextGA measurementId="G-5XP46M5HNL" />
+      <Component {...pageProps} />
+    </>
+  );
 }
-
-export default MyApp
